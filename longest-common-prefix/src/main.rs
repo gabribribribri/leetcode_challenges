e3decd68aa8050e45af3968f4f0b8075862dce63
gabribribribri <fuelanemo149@gmail.com>
2024-07-23 18:39:09 +0200

    pf. longest common prefix was so easy

---
+++ longest-common-prefix/src/main.rs
@@ -0,0 +1,24 @@
+struct Solution;
+fn main() {
+    println!("welp. not even a cargo test. sowwy");
+}
+impl Solution {
+    pub fn longest_common_prefix(strs: Vec<String>) -> String {
+        let mut common_prefix = String::new();
+        for i in 0.. {
+            let mut common_char: Option<char> = None;
+            for s in strs.iter() {
+                if let Some(ch) = s.chars().nth(i) {
+                    if ch != common_char.unwrap_or(ch) {
+                        return common_prefix;
+                    }
+                    common_char = Some(ch);
+                } else {
+                    return common_prefix;
+                }
+            }
+            common_prefix.push(common_char.unwrap())
+        }
+        common_prefix
+    }
+}
