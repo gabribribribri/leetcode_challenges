a6bb4899f4f9ff3bbe3de9c58bade0219165ab26
gabribribribri <fuelanemo149@gmail.com>
2024-07-26 22:46:22 +0200

    first attempt. not quite here yet

--- three-sum/src/main.rs
+++ three-sum/src/main.rs
@@ -8,13 +8,13 @@ fn main() {
 mod caligula {
     use super::*;
 
-    // #[test]
-    // fn three_sum_01() {
-    //     assert_eq!(
-    //         Solution::three_sum(vec![-1, 0, 1, 2, -1, -4]),
-    //         vec![vec![-1, 0, 1], vec![-1, -1, 2],]
-    //     )
-    // }
+    #[test]
+    fn three_sum_01() {
+        assert_eq!(
+            Solution::three_sum(vec![-1, 0, 1, 2, -1, -4]),
+            vec![vec![-1, 0, 1], vec![-1, -1, 2],]
+        )
+    }
 }
 
 struct Solution;
@@ -27,6 +27,7 @@ impl Solution {
         let mut l = m - 1;
         let mut r = m + 1;
         while m >= 1 && m <= nums.len() - 2 {
+            // debugging
             for n in &nums {
                 print!("{} ", n.abs());
             }
@@ -45,31 +46,46 @@ impl Solution {
                     }
                 )
             }
+            println!();
+            //-------------------------
             let triplet_value = nums[l] + nums[m] + nums[r];
             if triplet_value == 0 {
                 triplets.push(vec![nums[l], nums[m], nums[r]]);
                 if r == nums.len() - 1 {
-                    m += 1;
-                    l = m - 1;
-                    r = m + 1;
-                }
-                r += 1;
-            } else if triplet_value < 0 {
-                if l == 0 {
                     m -= 1;
+                    if m == 0 {
+                        break;
+                    }
                     l = m - 1;
                     r = m + 1;
+                } else {
+                    r += 1;
                 }
-                l -= 1;
             } else if triplet_value > 0 {
-                if r == nums.len() - 1 {
+                if l == 0 {
                     m += 1;
+                    if m == 0 {
+                        break;
+                    }
                     l = m - 1;
                     r = m + 1;
+                } else {
+                    l -= 1;
                 }
-                r += 1;
+            } else if triplet_value < 0 {
+                if r == nums.len() - 1 {
+                    m -= 1;
+                    if m == 0 {
+                        break;
+                    }
+                    l = m - 1;
+                    r = m + 1;
+                } else {
+                    r += 1;
+                }
             }
         }
+        triplets.dedup();
         return triplets;
     }
 }
