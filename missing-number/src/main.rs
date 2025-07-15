ed15cac53404cb2e816ced69b1c04666c5395cae
gabribribribri <fuelanemo149@gmail.com>
2025-07-15 19:52:54 +0200

    missing_number before XOR trick
    beats 100% speed and around 70% memory

---
+++ missing-number/src/main.rs
@@ -0,0 +1,48 @@
+#![allow(dead_code)]
+
+fn main() {
+    println!("Use `cargo test` to test implementation");
+}
+
+#[cfg(test)]
+mod garglurglug {
+    use super::*;
+
+    #[test]
+    fn missing_number_1() {
+        assert_eq!(Solution::missing_number(vec![3, 0, 1]), 2)
+    }
+
+    #[test]
+    fn missing_number_2() {
+        assert_eq!(Solution::missing_number(vec![0, 1]), 2)
+    }
+
+    #[test]
+    fn missing_number_3() {
+        assert_eq!(Solution::missing_number(vec![9,6,4,2,3,5,7,0,1]), 8)
+    }
+}
+
+struct Solution;
+
+struct HugeInt {
+    val: Vec<u8>
+}
+
+impl Solution {
+    pub fn missing_number(nums: Vec<i32>) -> i32 {
+        let mut flag = vec![0u8; nums.len()/8+1];
+
+        for n in nums {
+            flag[n as usize/8] |= 0b1 << n as usize%8;
+        }
+        
+        for (i, byte) in flag.iter().enumerate() {
+            if byte.trailing_ones() != 8 || i == flag.len()-1{
+                return (i as i32*8)+(byte.trailing_ones() as i32);
+            }
+        }
+        unreachable!()
+    }
+}
